080bb2e592f72862e3147bd8428de2a7c06e5bab
laura. <lauraospina21878@gmail.com>
2025-07-29 09:27:03 -0500

    index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import { createRoot } from "react-dom/client";
 import App from "./Componentes/App.js";
 
 const contenedor =document.getElementById('root')
-const root=createRoot(contenedor)
+f
 root.render(<App/>)
