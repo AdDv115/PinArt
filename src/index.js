39732779c43eeebcce22f4cd1a60ed5631b1e9c9
Adless <adeth115@gmail.com>
2025-07-29 09:15:37 -0500

    PinArt

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+import App from "./Componentes/App.js";
+
+const contenedor =document.getElementById('root')
+const root=createRoot(contenedor)
+root.render(<App/>)
